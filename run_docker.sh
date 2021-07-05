6fe8b7e98d3b671ebe01f241a162cb917ec6d762
Banjo <banjtheman@gmail.com>
2021-07-05 18:35:56 -0400

    make new dirs

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting Highwind docker image"
 mkdir -p highwind_jsons
+mkdir -p /home/highwind_jsons/items/
+mkdir -p /home/highwind_jsons/contracts/
 mkdir -p build
 docker run -d -p 8501:8501 -v $PWD/highwind_jsons:/home/highwind_jsons -v $PWD/build:/home/build highwind
 echo "Website live at localhost:8501"
